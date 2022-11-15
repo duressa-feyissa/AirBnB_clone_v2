55b63357b05741fb88b60fa89c28b82f6c5f0598
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-11-15 21:34:26 +0300

    alx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
-name="nginx"
-dpkg -s $name &> /dev/null
-if [ $? -ne 0 ]
-then
-    sudo apt-get update
-    sudo apt-get install $name
-fi
-FOLDER=("/data" "/data/web_static" "/data/web_static/releases"
-"/data/web_static/shared" "/data/web_static/releases/test")
-for val in ${FOLDER[@]};
-do
-    if [ ! -d $val ]
-    then
-        sudo mkdir $val
-    fi
-done
-file="/data/web_static/releases/test/index.html"
-source="/data/web_static/releases/test/"
-symbollink="/data/web_static/current"
-sudo echo "This is a test" | sudo tee $file
-sudo ln -sf $source $symbollink
-sudo chown -hR ubuntu:ubuntu /data
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo nginx -s reload
+sudo service nginx start
