149b44dfd6c1da2ce1c59ee4e83f911e166824f5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-11-18 02:09:04 +0300

    axl

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,7 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             if key in self.__objects:
-            	del self.__objects[key]
+                del self.__objects[key]
 
     def close(self):
         """ calls reload()
