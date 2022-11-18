e838a7823f450baeec363e08309396ade2f835bb
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-11-18 09:52:54 +0300

    Alx

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def city():
+    """ Route: /cities_by_states
+    """
+    data = storage.all("State")
+    return render_template("8-cities_by_states.html", states=data)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
